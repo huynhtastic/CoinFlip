6b0cf4c9275ee5de396d09af7d0168b8c68d9f86
Chris Spradling <cjspradling@gmail.com>
2016-04-18 22:35:57 -0500

    Cleanup
    Dropped extraneous files, started JSON parsing tool

--- app/src/main/java/cs329e/coinflip/YelpRequest.java
+++ app/src/main/java/cs329e/coinflip/YelpRequest.java
@@ -3,6 +3,19 @@ package cs329e.coinflip;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.json.JSONStringer;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.Map;
+
 /**
  * Created by cjs2599 on 4/18/16.
  */
@@ -15,6 +28,8 @@ public class YelpRequest extends AsyncTask<String, Void, String> {
     private static final String TOKEN = "8DRYE5aifjtJWMbC2LbeXojy2-0TYZsV";
     private static final String TOKEN_SECRET = "6qUlZk4pTtxyHyZuub3YSEKVrRc";
 
+    private Map<String, String> RESULT;
+
 
 
     @Override
@@ -26,14 +41,35 @@ public class YelpRequest extends AsyncTask<String, Void, String> {
     @Override
     protected String doInBackground(String... params) {
         YelpAPI api = new YelpAPI(CONSUMER_KEY, CONSUMER_SECRET, TOKEN, TOKEN_SECRET);
+
         return api.searchForBusinessesByLocation("tacos", "Austin");
     }
 
+
     @Override protected void onPostExecute(String resp) {
         Log.v("res", resp);
+        RESULT.put("key", "value");
+
     }
 
+    private void randomSelector(String response) {
+        JSONParser parser = new JSONParser();
 
+        try {
+
+            Object obj = parser.parse(response);
+
+            JSONObject json = (JSONObject) obj;
+
+            
+
+
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
 }
